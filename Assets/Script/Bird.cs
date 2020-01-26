f626a2d2197181af7326fdfc9ee7eb6e217fb6d6
Rajat Bansal <rajatbansal1012@gmail.com>
2020-01-26 15:48:27 +0530

    flappy bird game

---
+++ Assets/Script/Bird.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class Bird : MonoBehaviour 
+{
+    public float upForce = 200f;                    //Upward force of the "flap".
+    private bool isDead = false;            //Has the player collided with a wall?
+
+      private Animator anim;                    
+    private Rigidbody2D rb2d;                //Holds a reference to the Rigidbody2D component of the bird.
+
+    void Start()
+    {
+        anim = GetComponent<Animator> ();
+        //Get and store a reference to the Rigidbody2D attached to this GameObject.
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //Don't allow control if the bird has died.
+        if (isDead == false) 
+        {
+            //Look for input to trigger a "flap".
+            if (Input.GetMouseButtonDown(0)) 
+            {
+                //...zero out the birds current y velocity before...
+                rb2d.velocity = Vector2.zero;
+ 
+                //    new Vector2(rb2d.velocity.x, 0);
+                //..giving the bird some upward force.
+                rb2d.AddForce(new Vector2(0, upForce));
+                anim.SetTrigger("Flap");
+            }
+        }
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+      rb2d.velocity=Vector2.zero;
+      isDead = true;
+      anim.SetTrigger ("Die");
+        // If the bird collides with something set it to dead...
+       GameControl.instance.BirdDied(); 
+    }
+    
+}
---
+++ Assets/Script/GameControl.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour 
+{
+    public static GameControl instance;                        //A reference to the UI text component that displays the player's score.
+    public GameObject gameOvertext;                //A reference to the object that displays the text which appears when the player dies.
+    public Text scoreText;
+                            //The player's score.
+    public bool gameOver = false;                //Is the game over?
+   public float scrollSpeed = -1.5f;
+   private int score = 0;
+
+    void Awake()
+    {
+        //If we don't currently have a game control...
+        if (instance == null)
+            //...set this one to be it...
+            instance = this;
+        //...otherwise...
+        else if(instance != this)
+            //...destroy this one because it is a duplicate.
+            Destroy (gameObject);
+    }
+
+    void Update()
+    {
+        //If the game is over and the player has pressed some input...
+        if (gameOver == true && Input.GetMouseButtonDown(0)) 
+        {
+            //...reload the current scene.
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+    public void BirdScored()
+{
+     if(gameOver)
+     {
+       return;
+      }
+score++;
+scoreText.text = "Score: " + score.ToString ();
+}
+
+    public void BirdDied()
+    {
+        //Activate the game over text.
+        gameOvertext.SetActive (true);
+        //Set the game to be over.
+        gameOver = true;
+    }
+}
